89aeb8e285bb6e0578941f2736144652396c671a
Dinan <dinanvanw@gmail.com>
2024-11-22 15:26:14 +0100

    Fixed PlayerMovement
    Tweaked PlyerMovement, so it does have realistic 'earth-like' gravity.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,71 +4,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float Movement;
-    [SerializeField] float MovementSpeed;
-    [SerializeField] bool Jumped;
-    [SerializeField] float JumpHeight;
-    
-    GroundChecker GroundChecker;
-    Rigidbody Rb;
+    [SerializeField] float MovementSpeed = 5f;
+    [SerializeField] float JumpHeight = 5f;
+    private bool Jumped = false;
 
-    Camera Camera;
-    // Start is called before the first frame update
+    private GroundChecker GroundChecker;
+    private Rigidbody Rb;
+    private Camera Camera;
+
     void Start()
     {
         Rb = GetComponent<Rigidbody>();
-        
         GroundChecker = GetComponentInChildren<GroundChecker>();
         Camera = Camera.main;
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && GroundChecker.IsGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && GroundChecker.IsGrounded)
+        {
             Jumped = true;
         }
-        // if(!GroundChecker.IsGrounded) {
-        //     MovementSpeed *= 0.5f;
-        // }
     }
 
-    // FixedUpdate is called 50 * / second
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Vector3 cameraForward = Camera.transform.forward;
         Vector3 cameraRight = Camera.transform.right;
 
-        // Normalize to avoid diagonal speed boost
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-
-        // Zero out the y component to keep the movement on the XZ plane
+        // Zero out the y component to keep movement on the XZ plane
         cameraForward.y = 0;
         cameraRight.y = 0;
 
+        // Normalize directions
+        cameraForward.Normalize();
+        cameraRight.Normalize();
 
-        if(Jumped) {
-            Rb.AddForce(0, JumpHeight, 0, ForceMode.VelocityChange);
+        // Calculate movement direction
+        Vector3 movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += cameraForward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement -= cameraForward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement -= cameraRight;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += cameraRight;
+        }
+
+        // Scale movement by speed
+        movement = movement.normalized * MovementSpeed;
+
+        // Preserve the vertical velocity
+        float verticalVelocity = Rb.velocity.y;
+
+        // Apply jump force
+        if (Jumped)
+        {
+            verticalVelocity = JumpHeight;
             Jumped = false;
         }
 
-        Vector3 movement = Vector3.zero;
-
-        if(Input.GetKey(KeyCode.W)) {
-            movement += cameraForward * MovementSpeed;
-        }
-        if(Input.GetKey(KeyCode.S)) {
-            movement -= cameraForward * MovementSpeed;
-        }
-        if(Input.GetKey(KeyCode.A)) {
-            movement -= cameraRight * MovementSpeed;
-        }
-        if(Input.GetKey(KeyCode.D)) {
-            movement += cameraRight * MovementSpeed;
-        }
-
-        // Apply movement as a single force to the rigidbody
-        Rb.AddForce(movement, ForceMode.VelocityChange);
+        // Set the velocity, preserving the vertical component
+        Rb.velocity = new Vector3(movement.x, verticalVelocity, movement.z);
     }
 }
