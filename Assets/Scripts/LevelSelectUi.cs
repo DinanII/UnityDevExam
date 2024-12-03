b7b398e4b7d2779c30ace82c48568f9e737582db
Dinan <dinanvanw@gmail.com>
2024-12-03 15:45:36 +0100

    Main Menu: Byn for Main Lvl2 && Autorun Subtitle

--- Assets/Scripts/LevelSelectUi.cs
+++ Assets/Scripts/LevelSelectUi.cs
@@ -12,6 +12,11 @@ public class LevelSelect : MonoBehaviour
     public void LoadLevel0() {
         SceneManager.LoadScene("Level0");
     }
+
+    public void LoadLevel1() {
+        SceneManager.LoadScene("Level1");
+    }
+
     public void MainMenu() {
         SceneManager.LoadScene("MainMenu");
     }
